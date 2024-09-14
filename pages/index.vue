7699a9632f70e72e223eeca93d5dce15555e1b59
TSM-Odense-Bartech <tsm@odensebartech.com>
2024-09-14 21:44:29 +0200

    Changed

--- pages/index.vue
+++ pages/index.vue
@@ -119,11 +119,11 @@ export default {
 
     <!-- Content Container -->
     <div
-      class="max-w-6xl mx-auto px-10 py-20 flex flex-col items-center gap-10"
+      class="relative max-w-6xl mx-auto px-10 py-20 flex flex-col items-center gap-10 pt-[200px]"
     >
       <!-- Banner -->
       <div
-        class="w-[100vw] bg-primary flex flex-col items-center justify-center gap-2 py-10"
+        class="absolute top-10 w-[100vw] bg-primary flex flex-col items-center justify-center gap-2 py-10"
       >
         <h4 class="text-white text-2xl font-bold">MK1 Releases in</h4>
         <h3 class="text-white text-4xl md:text-5xl font-bold">March 2025</h3>
